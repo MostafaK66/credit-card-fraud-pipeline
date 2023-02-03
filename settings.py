e358146a6eb40985818f62d18f2e5701df75507e
MostafaK66 <mos.karimzadeh@gmail.com>
2023-02-03 11:38:25 +0100

    Add TODO reminders

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
-
+#TODO:Revert changes on parameters
 PATH_TO_READ_DATA = "/Users/mostafa_mac/Desktop/kaggle_datasets/creditcard.csv"
 OUT_PUT_PATH = os.path.join(os.getcwd(), "output")
 STRATIFIED_SPLITS = 5
